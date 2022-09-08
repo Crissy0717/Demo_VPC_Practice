8d97a8aa298e4ca5a900616f5261cdffe844dc6b
Christine Johnson <crissyjohnson@christines-air.myfiosgateway.com>
2022-09-07 21:43:46 -0400

    add

--- network.tf
+++ network.tf
@@ -2,6 +2,6 @@ resource "aws_vpc" "development-vpc" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true
   tags = {
-    Name = "${var.environment}-VPC"
+    Name = "${var.environment}-VPC-1"
   }
 }
