2b5d82a6b36405a4ca3571e7a47821b36e8f40f3
Christine Johnson <crissyjohnson@christines-air.myfiosgateway.com>
2022-09-08 20:14:00 -0400

    add

--- network.tf
+++ network.tf
@@ -8,3 +8,11 @@ resource "aws_vpc" "development-vpc" {
 
 #"${}"   #$ is used to call variables "${var.region}"
 #"${aws_vpc.development-vpc.id}"
+resource "aws_subnet" "public-subnet-1" {
+  cidr_block        = "${var.public_subnet_1_cidr}"
+  vpc_id            = "${aws_vpc.development-vpc.id}"
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "${var.environment}-crissy-vpc"
+  }
+}
